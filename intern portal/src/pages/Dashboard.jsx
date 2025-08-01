9f409c7588101241f42b32b61422af42b09799f3
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-01 16:17:58 +0530

    new commit

--- intern portal/src/pages/Dashboard.jsx
+++ intern portal/src/pages/Dashboard.jsx
@@ -1,2 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+export default function Dashboard() {
+    const [data, setData] = useState({});
+}
+
+useEffect (()=> {
+    axios.get("https://api.example.com/data")
+    .then(res => setData(res.data))
+    .catch(err => console.error(err));
+}, []);
+
+
+
+})
