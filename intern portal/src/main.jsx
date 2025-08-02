aa3374b69021747e9a099992a1ac86612a005c36
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-03 00:06:00 +0530

    new commit

--- intern portal/src/main.jsx
+++ intern portal/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ This is required
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ createRoot is now defined
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
