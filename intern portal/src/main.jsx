5dcad61526985eaab3e41c5345a8d0258898b290
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-03 23:14:02 +0530

    new commit

--- intern portal/src/main.jsx
+++ intern portal/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // ✅ This is required
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ createRoot is now defined
-root.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
